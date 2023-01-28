633959cee2e491e983d7788d2040241112e7070e
Amit Kumar Aich <87881212+NightCoderAmit@users.noreply.github.com>
2023-01-28 22:22:45 +0530

    error_removal

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useState, useEffect} from 'react';
 import "./App.css";
 import { uuid } from 'uuidv4';
